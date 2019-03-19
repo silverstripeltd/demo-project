8a58de7e8b65b5e9fa03e333ad113a78e583d314
Robin Barling <robin@silverstripe.com>
2019-03-20 12:57:03 +1300

    Add input fields that was missing in the CMS

--- app/src/ArticlePage.php
+++ app/src/ArticlePage.php
@@ -8,6 +8,9 @@ use SilverStripe\Assets\File;
 use SilverStripe\AssetAdmin\Forms\UploadField;
 use SilverStripe\Forms\CheckboxSetField;
 use SilverStripe\Forms\DropdownField;
+use SilverStripe\Forms\TextareaField;
+use SilverStripe\Forms\TextField;
+use SilverStripe\Forms\DateField;
 
 class ArticlePage extends Page
 {
@@ -40,12 +43,22 @@ class ArticlePage extends Page
     {
         $fields = parent::getCMSFields();
 
+        $fields->addFieldToTab('Root.Main', DateField::create('Date','Date of article'), 'Content');
+
         $fields->addFieldToTab('Root.Attachments', $photo = UploadField::create('Photo'));
         $fields->addFieldToTab('Root.Attachments',
             $brochure = UploadField::create('Brochure', 'Travel brochure, optional (PDF only)'));
 
         $photo->getValidator()->setAllowedExtensions(['png', 'gif', 'jpg', 'jpeg']);
         $photo->setFolderName('travel-photos');
+
+
+        $fields->addFieldToTab('Root.Main', TextareaField::create('Teaser')
+            ->setDescription('This is the summary that appears on the article list page.'),
+            'Content'
+        );
+
+        $fields->addFieldToTab('Root.Main', TextField::create('Author','Author of article'),'Content');
 
         $brochure->getValidator()->setAllowedExtensions(['pdf']);
         $brochure->setFolderName('travel-brochures');
