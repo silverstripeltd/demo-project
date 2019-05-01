84016ccb343c043488e3712529841f5489143f17
Robin Barling <robin@silverstripe.com>
2019-05-02 09:07:13 +1200

    Removed fields for the purpose of demo. Fields will be added by other branches.

--- app/src/ArticlePage.php
+++ app/src/ArticlePage.php
@@ -14,13 +14,6 @@ use SilverStripe\Forms\DateField;
 
 class ArticlePage extends Page
 {
-
-    private static $db = [
-        'Date' => 'Date',
-        'Teaser' => 'Text',
-        'Author' => 'Varchar(50)'
-    ];
-
     private static $has_one = [
         'Photo' => Image::class,
         'Brochure' => File::class,
@@ -43,22 +36,12 @@ class ArticlePage extends Page
     {
         $fields = parent::getCMSFields();
 
-        $fields->addFieldToTab('Root.Main', DateField::create('Date','Date of article'), 'Content');
-
         $fields->addFieldToTab('Root.Attachments', $photo = UploadField::create('Photo'));
         $fields->addFieldToTab('Root.Attachments',
             $brochure = UploadField::create('Brochure', 'Travel brochure, optional (PDF only)'));
 
         $photo->getValidator()->setAllowedExtensions(['png', 'gif', 'jpg', 'jpeg']);
         $photo->setFolderName('travel-photos');
-
-
-        $fields->addFieldToTab('Root.Main', TextareaField::create('Teaser')
-            ->setDescription('This is the summary that appears on the article list page.'),
-            'Content'
-        );
-
-        $fields->addFieldToTab('Root.Main', TextField::create('Author','Author of article'),'Content');
 
         $brochure->getValidator()->setAllowedExtensions(['pdf']);
         $brochure->setFolderName('travel-brochures');
